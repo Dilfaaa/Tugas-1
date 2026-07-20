c948bf2deb9d1d18f62c229aba4f207422fb25e3
FadilYusril <yusrilneneknu51@gmail.com>
2026-07-20 16:07:09 +0800

    revisi sedikit

--- studikasus2.js
+++ studikasus2.js
@@ -1,11 +1,17 @@
 const nilai = [90, 75, 60, 45, 88];
+let siswaLulus = 0
+let siswaTidakLulus = 0
 for (let i = 0; i < nilai.length; i++) {
     let status = ``
     if (nilai[i] >= 75) {
         status = `lulus`
+        siswaLulus++
     }
     else if (nilai[i] < 75) {
         status = `tidak lulus`
+        siswaTidakLulus++
     }
     console.log(`Siswa ke-${i+1} : ${nilai[i]} -> ${status}`)
 }
+console.log(`siswa yang lulus: ${siswaLulus}`)
+console.log(`siswa yang tidak lulus: ${siswaTidakLulus}`)
